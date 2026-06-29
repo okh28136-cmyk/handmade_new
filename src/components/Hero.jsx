4cc011b888c60d9c797269f5472b9a49bd9d056f
okh28136-cmyk <okh28136@gmail.com>
2026-06-29 18:25:43 +0900

    feat: 메인 배너(Hero) 배경을 유튜브 영상으로 교체 (우측 하단 워터마크 완벽 가림 처리)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,10 +22,15 @@ const Hero = () => {
         </div>
       </div>
       <div className="hero-image-wrapper">
-        <div 
-          className="hero-bg-layer"
-          style={{ backgroundImage: `url(/hero-bg-1.jpg)` }}
-        />
+        <div className="hero-bg-layer">
+          <iframe
+            className="hero-video-iframe"
+            src="https://www.youtube.com/embed/m47f9IhwWuk?autoplay=1&mute=1&controls=0&loop=1&playlist=m47f9IhwWuk&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&disablekb=1"
+            frameBorder="0"
+            allow="autoplay; encrypted-media"
+            title="Background Video"
+          ></iframe>
+        </div>
         {/* 가독성을 높이기 위한 어두운 오버레이 */}
         <div className="hero-overlay"></div>
         <div className="hero-overlay-text font-playfair">
